aa422d122ed3ab69cbb6d2f34053f39e25848825
John Layher <johnevanlayher@gmail.com>
2023-08-03 17:58:07 -0400

    Created Basic Layout

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './footer.css'
+
+
+const Footer = () => {
+  return (
+    <div className='footer_container'>
+      <div className='footer_container-title'>This is the footer!</div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './header.css'
+
+
+const Header = () => {
+  return (
+    <div className='header_container'>
+        <div className='header_container-text'>The Dogs of Dr. John Layher</div>
+    </div>
+  )
+}
+
+export default Header
