886f9b99bec3f29cb69e021660057d469edcc90d
John Layher <johnevanlayher@gmail.com>
2023-08-03 23:41:18 -0400

    updated css, and positioned cards

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -10,7 +10,7 @@ const Card = () => {
         <img src={lemon} alt="lemon" />
         <div className='card-container'>
           <h4>Dog Name</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
         </div>
       </div>
     </>
